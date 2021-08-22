98da96866c4868cf383bb09a3b3c1a080ce7876d
weichee98 <YEWW0006@e.ntu.edu.sg>
2021-08-22 19:36:14 +0800

    added files fo models (GCN, FFN) and their corresponding train and test steps

---
+++ src/models/FFN.py
@@ -0,0 +1,58 @@
+import torch
+import torch.nn.functional as F
+
+
+class FFN(torch.nn.Module):
+    
+    def __init__(self, input_size, l1, l2):
+        super().__init__()
+        self.linear1 = torch.nn.Linear(input_size, l1)
+        self.linear2 = torch.nn.Linear(l1, l2)
+        self.linear3 = torch.nn.Linear(l2, 2) # this is the head for disease class
+            
+    def forward(self, x): # full batch
+        x = self.linear1(x)
+        x = F.relu(x)
+        x = F.dropout(x, p=0.5, training=self.training)
+        
+        x = self.linear2(x)
+        x = F.relu(x)
+        x = F.dropout(x, p=0.5, training=self.training)
+        
+        x = self.linear3(x)
+        x = F.softmax(x, dim=1) # output for disease classification        
+        return x
+
+
+def train_FFN(device, model, data, optimizer, train_idx):
+    model.to(device)
+    model.train()
+    optimizer.zero_grad()  # Clear gradients
+
+    pred_y = model(data.x.to(device))
+    real_y = data.y[train_idx].to(device)
+    criterion = torch.nn.CrossEntropyLoss()
+    loss = criterion(pred_y[train_idx], real_y)
+    loss_val = loss.item()
+    loss.backward()     # Derive gradients.
+    optimizer.step()    # Update parameters based on gradients.
+    
+    pred = pred_y.argmax(dim=1)
+    correct = pred[train_idx] == real_y
+    accuracy = correct.float().mean()
+    return loss_val, accuracy.item()
+
+
+def test_FFN(device, model, data, test_idx):
+    model.to(device)
+    model.eval()
+
+    pred_y = model(data.x.to(device))
+    real_y = data.y[test_idx].to(device)
+    criterion = torch.nn.CrossEntropyLoss()
+    loss = criterion(pred_y[test_idx], real_y)
+    
+    pred = pred_y.argmax(dim=1)
+    correct = pred[test_idx] == real_y
+    accuracy = correct.float().mean()
+    return loss.item(), accuracy.item()
