6e3bf860ecdad6b52e033c63a34d6aa3bf9b1eb2
weichee98 <YEWW0006@e.ntu.edu.sg>
2021-09-16 19:08:00 +0800

    included sensitivity, specificity and f1 in training

--- src/models/VAE.py
+++ src/models/VAE.py
@@ -7,6 +7,7 @@ __dir__ = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(__dir__)
 
 from utils.loss import GaussianKLDivLoss
+from utils.metrics import ClassificationMetrics as CM
 
 
 class VAE(torch.nn.Module):
@@ -108,9 +109,7 @@ def train_VAE(
     loss.backward()
     optimizer.step()
     
-    pred = pred_y.argmax(dim=1)
-    correct = pred[labeled_idx] == real_y
-    accuracy = correct.float().mean()
+    accuracy = CM.accuracy(real_y, pred_y[labeled_idx])
     return loss_val, accuracy.item()
 
 
@@ -122,8 +121,15 @@ def test_VAE(device, model, data, test_idx):
     real_y = data.y[test_idx].to(device)
     criterion = torch.nn.CrossEntropyLoss()
     loss = criterion(pred_y[test_idx], real_y)
-    
-    pred = pred_y.argmax(dim=1)
-    correct = pred[test_idx] == real_y
-    accuracy = correct.float().mean()
-    return loss.item(), accuracy.item(), {}
+
+    pred_y = pred_y.argmax(dim=1)[test_idx]
+    accuracy = CM.accuracy(real_y, pred_y)
+    sensitivity = CM.tpr(real_y, pred_y)
+    specificity = CM.tnr(real_y, pred_y)
+    f1_score = CM.f1_score(real_y, pred_y)
+    metrics = {
+        "sensitivity": sensitivity.item(),
+        "specificity": specificity.item(),
+        "f1": f1_score.item()
+    }
+    return loss.item(), accuracy.item(), metrics
