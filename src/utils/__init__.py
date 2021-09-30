8ce271c37a0dfb7ce0fc39c64bb9eb55005ebb0c
weichee98 <YEWW0006@e.ntu.edu.sg>
2021-09-30 17:03:54 +0800

    update utils

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,6 +1,10 @@
 import os
 import time
+import random
 import traceback
+import numpy as np
+import torch
+from tqdm import tqdm
 
 
 def mkdir(path):
@@ -26,7 +30,6 @@ def on_error(value, print_error_stack=True):
     return decorator
 
 
-
 def log_time(function):
     def wrapper(*args, **kwargs):
         start = time.time()
@@ -38,4 +41,45 @@ def log_time(function):
         )
         return res
     return wrapper
-    
+
+
+def get_device(id):
+    if id >= 0 and torch.cuda.is_available():
+        print("Using device: cuda:{}".format(id))
+        device = torch.device("cuda:{}".format(id))
+    else:
+        print("Using device: cpu")
+        device = torch.device("cpu")
+    return device
+
+
+def seed_torch(seed=42):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+
+
+def verbose_info(epoch, train_loss, test_loss, train_acc, test_acc):
+    return "Epoch: {:03d}, Train Acc: {:.4f}, Test Acc: {:.4f}, " \
+        "Train Loss: {:.4f}, Test Loss: {:.4f}".format(
+            epoch, train_acc, test_acc, train_loss, test_loss
+        )
+
+
+def epoch_gen(max_epoch=1000):
+    i = 1
+    while i <= max_epoch:
+        yield i
+        i += 1
+
+
+def get_pbar(max_epoch, verbose):
+    if verbose:
+        return tqdm(epoch_gen(max_epoch))
+    else:
+        return epoch_gen(max_epoch)
