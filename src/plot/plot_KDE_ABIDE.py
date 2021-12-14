1574dc5ebbdc4e1b9dff9a2d6ce930d3de2f3ff3
weichee98 <YEWW0006@e.ntu.edu.sg>
2021-12-14 17:39:37 +0800

    update plotting script

--- src/plot/plot_KDE_ABIDE.py
+++ src/plot/plot_KDE_ABIDE.py
@@ -19,9 +19,13 @@ def plot_pairwise_site(X, y, sites, columns=None, name="site_distribution"):
     f.savefig("{}.png".format(name))
 
     is_diseased = y[:, 1] == 1
-    site_mean_control = SD.get_site_mean(X[~is_diseased], sites[~is_diseased], fisher=True)
-    site_mean_diseased = SD.get_site_mean(X[is_diseased], sites[is_diseased], fisher=True)
-    
+    site_mean_control = SD.get_site_mean(
+        X[~is_diseased], sites[~is_diseased], fisher=True
+    )
+    site_mean_diseased = SD.get_site_mean(
+        X[is_diseased], sites[is_diseased], fisher=True
+    )
+
     site_mean = dict()
     for s in np.unique(sites):
         site_mean[s] = dict()
@@ -53,28 +57,25 @@ def plot_pairwise_group(X, y, sites, group_ids, name="group_distribution"):
     group_names = defaultdict(list)
     for site, group in group_ids.items():
         group_names[group].append(site)
-    print(group_names)
+    for group, gsites in group_names.items():
+        group_names[group] = ", ".join(sorted(gsites))
 
-    group_legend = "\n".join([
-        "Group {}:\n{}\n".format(k, group_names[k])
-        for k in sorted(group_names)
-    ])
-    
-    def get_group_name(site):
-        return "Group {}".format(group_ids[site])
-    groups = np.vectorize(get_group_name)(sites)
-    group_order = ["Group {}".format(g) for g in sorted(group_names)]
+    func = lambda site: group_names[group_ids[site]]
+    groups = np.vectorize(func)(sites)
+    group_order = sorted(group_names.values())
     group_mean = SD.get_site_mean(X, groups, fisher=True)
     f = plot_group_kde(
-        group_mean, num_process=10, verbose=True, 
-        group_order=group_order
+        group_mean, num_process=10, verbose=True, group_order=group_order
     )
-    f.text(0.1, 0.1, group_legend)
     f.savefig("{}.png".format(name))
 
     is_diseased = y[:, 1] == 1
-    group_mean_control = SD.get_site_mean(X[~is_diseased], groups[~is_diseased], fisher=True)
-    group_mean_diseased = SD.get_site_mean(X[is_diseased], groups[is_diseased], fisher=True)
+    group_mean_control = SD.get_site_mean(
+        X[~is_diseased], groups[~is_diseased], fisher=True
+    )
+    group_mean_diseased = SD.get_site_mean(
+        X[is_diseased], groups[is_diseased], fisher=True
+    )
     group_mean = dict()
     for s in np.unique(groups):
         group_mean[s] = dict()
@@ -85,7 +86,6 @@ def plot_pairwise_group(X, y, sites, group_ids, name="group_distribution"):
     f = plot_group_kde(
         group_mean, num_process=10, verbose=True, group_order=group_order
     )
-    f.text(0.1, 0.1, group_legend)
     f.savefig("{}_with_classes.png".format(name))
 
 
@@ -98,7 +98,9 @@ def get_groups(X, sites):
     valid_sites = [site for site in groups if groups[site] == max_group]
 
     valid_idx = np.isin(sites, valid_sites)
-    groups = SD.get_site_grouping(X[valid_idx], sites[valid_idx], SD.METRIC.HELLINGER, SD.METHOD.KDE, 0.063)
+    groups = SD.get_site_grouping(
+        X[valid_idx], sites[valid_idx], SD.METRIC.HELLINGER, SD.METHOD.KDE, 0.063
+    )
     return groups
 
 
@@ -111,10 +113,26 @@ if __name__ == "__main__":
     X_harmonized, _ = load_data_fmri(harmonized=True)
     sites = get_sites()
     columns = [
-        "CMU", "OHSU", "YALE", "LEUVEN_1", "LEUVEN_2",
-        "UCLA_2", "UCLA_1", "SBL", "TRINITY", "SDSU",
-        "OLIN", "PITT", "KKI", "STANFORD", "CALTECH",
-        "UM_1", "UM_2", "MAX_MUN", "NYU", "USM"
+        "CMU",
+        "OHSU",
+        "YALE",
+        "LEUVEN_1",
+        "LEUVEN_2",
+        "UCLA_2",
+        "UCLA_1",
+        "SBL",
+        "TRINITY",
+        "SDSU",
+        "OLIN",
+        "PITT",
+        "KKI",
+        "STANFORD",
+        "CALTECH",
+        "UM_1",
+        "UM_2",
+        "MAX_MUN",
+        "NYU",
+        "USM",
     ]
 
     # plot_pairwise_site(X, y, sites, columns, "site_distribution")
@@ -122,7 +140,5 @@ if __name__ == "__main__":
 
     group_ids = get_groups(X, sites)
     plot_pairwise_group(X, y, sites, group_ids, "group_distribution")
-    plot_pairwise_group(X_harmonized, y, sites, group_ids, "group_distribution_combat")
-    
+    # plot_pairwise_group(X_harmonized, y, sites, group_ids, "group_distribution_combat")
 
-    
--- src/plot/plot_heatmap_ABIDE.py
+++ src/plot/plot_heatmap_ABIDE.py
@@ -4,10 +4,11 @@ import numpy as np
 import pandas as pd
 import seaborn as sb
 import matplotlib.pyplot as plt
+from collections import defaultdict
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from ABIDE import load_data_fmri, get_sites
+from ABIDE import get_labelling_standards, load_data_fmri, get_sites
 from utils.distribution import SiteDistribution
 
 
@@ -18,12 +19,9 @@ def sorting(df):
     return columns
 
 
-def plot_heatmap(df, metric):
-    f, ax = plt.subplots(1, 1, figsize=(15, 15))
-    sb.heatmap(
-        df, cmap="Blues", square=True, annot=True, 
-        fmt=".3f", ax=ax, cbar=False
-    )
+def plot_heatmap(df, metric, figsize=(15, 15)):
+    f, ax = plt.subplots(1, 1, figsize=figsize)
+    sb.heatmap(df, cmap="Blues", square=True, annot=True, fmt=".3f", ax=ax, cbar=False)
     ax.set_xlabel("P")
     ax.set_ylabel("Q")
     if metric == SD.METRIC.KL:
@@ -38,14 +36,41 @@ def plot_heatmap(df, metric):
     return f
 
 
-def plot_distribution(X, sites, metric, method, columns=None, **kwargs):
+def plot_distribution(
+    X, sites, metric, method, columns=None, figsize=(15, 15), **kwargs
+):
     dist_diff = SD.distribution_heatmap(X, sites, metric, method, **kwargs)
     dist_diff = pd.DataFrame(dist_diff)
     if columns is None:
         columns = sorting(dist_diff)
     dist_diff = dist_diff.loc[columns, :][columns]
-    f = plot_heatmap(dist_diff, metric)
+    f = plot_heatmap(dist_diff, metric, figsize)
     return f
+
+
+def get_groups(X, sites):
+    groups = get_labelling_standards()
+    groups.pop("CMU")
+
+    group_ids = list(groups.values())
+    max_group = max(set(group_ids), key=group_ids.count)
+    valid_sites = [site for site in groups if groups[site] == max_group]
+
+    valid_idx = np.isin(sites, valid_sites)
+    X = X[valid_idx]
+    sites = sites[valid_idx]
+    groups = SD.get_site_grouping(X, sites, SD.METRIC.HELLINGER, SD.METHOD.KDE, 0.063)
+
+    group_sites = defaultdict(list)
+    for site, group_id in groups.items():
+        group_sites[group_id].append(site)
+    for group_id, g_sites in group_sites.items():
+        group_sites[group_id] = ", ".join(sorted(g_sites))
+
+    func = lambda x: group_sites[groups[x]]
+    group_order = sorted(group_sites.values())
+    subject_groups = np.vectorize(func)(sites)
+    return X, subject_groups, group_order
 
 
 if __name__ == "__main__":
@@ -58,19 +83,46 @@ if __name__ == "__main__":
     SD = SiteDistribution()
 
     columns = [
-        "CMU", "OHSU", "YALE", "LEUVEN_1", "LEUVEN_2",
-        "UCLA_2", "UCLA_1", "SBL", "TRINITY", "SDSU",
-        "OLIN", "PITT", "KKI", "STANFORD", "CALTECH",
-        "UM_1", "UM_2", "MAX_MUN", "NYU", "USM"
+        "CMU",
+        "OHSU",
+        "YALE",
+        "LEUVEN_1",
+        "LEUVEN_2",
+        "UCLA_2",
+        "UCLA_1",
+        "SBL",
+        "TRINITY",
+        "SDSU",
+        "OLIN",
+        "PITT",
+        "KKI",
+        "STANFORD",
+        "CALTECH",
+        "UM_1",
+        "UM_2",
+        "MAX_MUN",
+        "NYU",
+        "USM",
     ]
 
-    for metric in SD.METRIC:
-        for method in SD.METHOD:
-            try:
-                f = plot_distribution(
-                    X, sites, metric, method,
-                    bins=1000, columns=columns
-                )
-                f.savefig("distribution_{}_{}.png".format(metric, method))
-            except Exception as e:
-                print(e)
+    # for metric in SD.METRIC:
+    #     for method in SD.METHOD:
+    #         try:
+    #             f = plot_distribution(
+    #                 X, sites, metric, method, bins=1000, columns=columns
+    #             )
+    #             f.savefig("distribution_{}_{}.png".format(metric, method))
+    #         except Exception as e:
+    #             print(e)
+
+    X, subject_groups, group_order = get_groups(X, sites)
+    f = plot_distribution(
+        X,
+        subject_groups,
+        SD.METRIC.HELLINGER,
+        SD.METHOD.KDE,
+        bins=1000,
+        figsize=(10, 10),
+        columns=group_order,
+    )
+    f.savefig("distribution_group_heatmap.png")
